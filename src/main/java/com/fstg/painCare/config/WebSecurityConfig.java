b3ab29dfbf37653cdbb963dd5066fb338ec453e8
ismail telhouni <ismailtelhouni123@gmail.com>
2024-01-08 23:03:30 +0100

    config spring security

--- src/main/java/com/fstg/painCare/config/WebSecurityConfig.java
+++ src/main/java/com/fstg/painCare/config/WebSecurityConfig.java
@@ -1,5 +1,7 @@
 package com.fstg.painCare.config;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +15,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.fstg.painCare.config.jwt.AuthEntryPointJwt;
 import com.fstg.painCare.config.jwt.AuthTokenFilter;
@@ -65,10 +70,10 @@ public class WebSecurityConfig {
 //		(stateless), ce qui est souvent le cas pour les API REST.
 //		Configure les autorisations pour différentes requêtes HTTP:
 		
-	    http.csrf(csrf -> csrf.disable())
+		http.csrf(csrf -> csrf.disable()).cors().and()
 	        .exceptionHandling(exception -> exception.authenticationEntryPoint(unauthorizedHandler))
 	        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-	        .authorizeHttpRequests(auth -> 
+	        .authorizeRequests(auth -> 
 	          auth.antMatchers("/api/auth/**").permitAll()
 	              .antMatchers("/api/test/**").permitAll()
 	              .anyRequest().authenticated()
@@ -80,5 +85,17 @@ public class WebSecurityConfig {
 	    
 	    return http.build();
 	  }
-	
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+	    CorsConfiguration configuration = new CorsConfiguration();
+	    configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200/"));
+	    configuration.setAllowedMethods(Arrays.asList("*"));
+	    configuration.setAllowedHeaders(Arrays.asList("*"));
+	    
+	    
+
+	    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	    source.registerCorsConfiguration("/**", configuration);
+	    return source;
+	}
 }
--- src/main/java/com/fstg/painCare/controllers/DiagnosticController.java
+++ src/main/java/com/fstg/painCare/controllers/DiagnosticController.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
